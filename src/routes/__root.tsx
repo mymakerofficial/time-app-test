33d57e24e1fe7f04cab24f1c285873fdf092331c
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-08-07 14:53:36 +0200

    init

---
+++ src/routes/__root.tsx
@@ -0,0 +1,60 @@
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+import Header from '../components/Header'
+
+import TanStackQueryLayout from '../integrations/tanstack-query/layout.tsx'
+
+import appCss from '../styles.css?url'
+
+import type { QueryClient } from '@tanstack/react-query'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+
+  shellComponent: RootDocument,
+})
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <Header />
+        {children}
+        <TanStackRouterDevtools />
+        <TanStackQueryLayout />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
