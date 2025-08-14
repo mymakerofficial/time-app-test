41d83213b6b6a8deea604bacb932d55879c5d1e4
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-08-14 14:20:55 +0200

    nicer

--- apps/backend/src/services.ts
+++ apps/backend/src/services.ts
@@ -2,8 +2,20 @@ import { JwtService } from '@/modules/jwt/service.ts'
 import { AuthService } from '@/modules/auth/service.ts'
 import { Elysia } from 'elysia'
 import { Container } from '@/lib/container.ts'
+import { Pool } from 'pg'
+import { drizzle } from '@/lib/drizzle.ts'
+import * as schema from '@/db/schema/schema.ts'
+
+const pool = new Pool({
+  connectionString:
+    'postgres://postgres_user:password@localhost:5432/postgres_db',
+})
+const poolClient = await pool.connect()
+const db = drizzle({ client: poolClient, schema })
+export type DB = typeof db
 
 export const container = new Container()
+  .add('db', () => db)
   .add('jwtService', () => new JwtService())
   .add('authService', (container) => new AuthService(container))
 
