9891e8447f61f6a1536626f37ef513e6ef9ed7a6
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-08-15 23:57:36 +0200

    i forgor

--- apps/backend/src/domain/timeEntries/controller.ts
+++ apps/backend/src/domain/timeEntries/controller.ts
@@ -5,45 +5,41 @@ import { t } from 'elysia'
 export const timeEntriesController = createApiController({
   prefix: '/time-entries',
   detail: { tags: ['Time Entries'] },
-})
-  .onBeforeHandle(async ({ session }) => {
-    await session.validateSession()
-  })
-  .get(
-    '/range',
-    ({ timeEntriesService, query: { start, end } }) => {
-      const stream = timeEntriesService.getRange({ start, end })
+}).get(
+  '/range',
+  ({ timeEntriesService, query: { start, end } }) => {
+    const stream = timeEntriesService.getRange({ start, end })
 
-      return new Response(stream, {
-        headers: {
-          'Content-Type': 'application/msgpack',
-        },
-      })
-    },
-    {
-      query: TimeEntriesModel.RangeQuery,
-      response: {
-        200: t.Uint8Array(),
+    return new Response(stream, {
+      headers: {
+        'Content-Type': 'application/msgpack',
       },
-      detail: {
-        responses: {
-          200: {
-            description: 'A stream of time entries encoded using MessagePack.',
-            content: {
-              'application/msgpack': {},
-            },
+    })
+  },
+  {
+    query: TimeEntriesModel.RangeQuery,
+    response: {
+      200: t.Uint8Array(),
+    },
+    detail: {
+      responses: {
+        200: {
+          description: 'A stream of time entries encoded using MessagePack.',
+          content: {
+            'application/msgpack': {},
           },
-          500: {
-            $ref: '#/components/schemas/error',
-            content: {
-              'application/json': {
-                schema: {
-                  $ref: '#/components/schemas/error',
-                },
+        },
+        500: {
+          $ref: '#/components/schemas/error',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/error',
               },
             },
           },
         },
       },
     },
-  )
+  },
+)
