e248c4ea3fda3bf50004f7822fef2d1674c1dcd2
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-08-19 14:15:03 +0200

    whoops

--- apps/backend/src/domain/model/timeEntries.ts
+++ apps/backend/src/domain/model/timeEntries.ts
@@ -1,5 +1,4 @@
 import z from 'zod'
-import { integer } from 'drizzle-orm/pg-core'
 import { uInt8Array } from '@time-app-test/shared/zod.ts'
 
 export const RangeSchema = z.object({
@@ -12,7 +11,7 @@ export const EncryptedTimeEntrySchema = z.object({
   id: z.nanoid(),
   createdAt: uInt8Array(),
   updatedAt: uInt8Array(),
-  lookupKey: integer().notNull(),
+  lookupKey: z.int(),
   startedAt: uInt8Array(),
   endedAt: uInt8Array(),
   message: uInt8Array(),
