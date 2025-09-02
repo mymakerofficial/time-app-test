5c5a9920741aed441850821ccd112554fa811b8a
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-09-02 13:42:33 +0200

    add Prettify type

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -3,3 +3,7 @@ export type MaybePromise<T> = T | Promise<T>
 export type MaybeFunction<TRes, TArgs extends any[] = any[]> =
   | TRes
   | ((...args: TArgs) => TRes)
+
+export type Prettify<T> = {
+  [K in keyof T]: T[K]
+} & {}
