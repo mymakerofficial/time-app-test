7c83c702bc16f540bbd6490645a54ae2a7232594
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-09-02 18:00:46 +0200

    use zod encode for response

--- apps/backend/src/adapter/rest/utils/zodAdapter.ts
+++ apps/backend/src/adapter/rest/utils/zodAdapter.ts
@@ -1,5 +1,18 @@
 import { z } from 'zod'
 import { ApiErrorResponseSchema } from '@time-app-test/shared/error/schema.ts'
+import { DocumentDecoration, LocalHook } from 'elysia/types'
+
+type AnyLocalHook = LocalHook<
+  {},
+  {},
+  {
+    decorator: {}
+    store: {}
+    derive: {}
+    resolve: {}
+  },
+  {}
+>
 
 function toSchema(schema: z.ZodType) {
   return z.toJSONSchema(schema, {
@@ -23,61 +36,76 @@ export function createLocalHook<
   body?: TBodySchema
   response?: TResponseSchema
   validateSession?: TValidateSession
+  encode?: boolean
 }): {
   body: TBodySchema
   response: TResponseSchema
   validateSession: TValidateSession extends true ? true : undefined
+  detail: DocumentDecoration
 } {
+  const encode = options.encode || true
+
   return {
-    ...(options.validateSession ? { validateSession: true } : {}),
-    body: options.body,
-    response: options.response,
-    detail: {
-      ...(options.body
-        ? {
-            requestBody: {
-              content: {
-                'application/json': {
-                  schema: toSchema(options.body),
-                },
+    ...(options.validateSession && {
+      validateSession: true as TValidateSession extends true ? true : undefined,
+    })!,
+    body: options.body!,
+    response: options.response!,
+    ...(options.response &&
+      encode && {
+        afterHandle: (async ({ response }) => {
+          return new Response(
+            JSON.stringify(await options.response!.encodeAsync(response)),
+            {
+              headers: {
+                'Content-Type': 'application/json',
               },
             },
-          }
-        : {}),
+          )
+        }) satisfies AnyLocalHook['afterHandle'],
+      }),
+    detail: {
+      ...(options.body && {
+        requestBody: {
+          content: {
+            'application/json': {
+              schema: toSchema(options.body),
+            },
+          },
+        },
+      }),
       responses: {
-        ...(options.response
-          ? {
-              200: {
-                content: {
-                  'application/json': {
-                    schema: toSchema(options.response),
-                  },
-                },
+        ...(options.response && {
+          200: {
+            content: {
+              'application/json': {
+                schema: toSchema(options.response),
               },
-            }
-          : {}),
+            },
+            description: '',
+          },
+        }),
         500: {
           content: {
             'application/json': {
               schema: toSchema(ApiErrorResponseSchema),
             },
           },
+          description: 'Something went wrong.',
         },
       },
-      ...(options.validateSession
-        ? {
-            parameters: [
-              {
-                schema: {
-                  type: 'string',
-                  pattern: '^Bearer .+$',
-                },
-                in: 'header',
-                name: 'authorization',
-              },
-            ],
-          }
-        : {}),
+      ...(options.validateSession && {
+        parameters: [
+          {
+            schema: {
+              type: 'string',
+              pattern: '^Bearer .+$',
+            },
+            in: 'header',
+            name: 'authorization',
+          },
+        ],
+      }),
     },
   }
 }
